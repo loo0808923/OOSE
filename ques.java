3af9481b90289ba04f67c1ec1d93529c09493b1c
loo0808923 <loo0808923@gmail.com>
2015-12-22 00:14:54 +0800

    6a

--- ques.java
+++ ques.java
@@ -1,6 +1,45 @@
 package hw6a;
+import java.util.ArrayList;
+import java.util.ListIterator;
 
+abstract class component
+{
+	     abstract void operation();
+}
+
+class composite extends component
+{
+	  ArrayList<component> list;
+	  public composite()
+	  {
+		     list = new ArrayList<component>();
+	  }
+	  void addquestion(component ques)
+	  {
+		   list.add(ques);
+	  }
+	  void operation()
+	  {
+		   ListIterator<component> iterator = list.listIterator();
+		   while(iterator.hasNext())
+		   {
+			     component ques = iterator.next();
+			     ques.operation();
+		   }
+	  }
+}
 public class ques
 {
-	   //a
+	   public static void main(String args[])
+	   {
+		      //composite c = new composite("ques1");
+		      component[] ques;
+	   }
+	   void clientop(component[] ques)
+	   {
+		    for(component q: ques)
+		    {
+		    	q.operation();
+		    }
+	   }
 }
